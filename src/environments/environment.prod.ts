f40a4228a87cd3e3f5c711f5f66087f100d64f30
Shanushajahan707 <shanushajahan707@gmail.com>
2024-07-03 20:15:41 +0530

    production file created

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:3000/',
+    GOOGLE_URL : 'https://accounts.google.com/o/oauth2/v2/auth?',
+    razorKey:"rzp_test_I5igMZcnHQiPyk",
+    paypalClientId:'AdTF00jjUqvWJuDk1wO2JKtxtQq8FKGl6z1kF7kABDBPeDnL0txFhhZ8-fK3SoE5pd6ndlyxtML2qoXE'
+  };
+  
