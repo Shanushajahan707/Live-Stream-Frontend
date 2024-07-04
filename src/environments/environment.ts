cbf879c61ab55152a11fb9b185d69845643f9524
Shanushajahan707 <shanushajahan707@gmail.com>
2024-07-04 12:14:42 +0530

    simple changes 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/',
+    apiUrl: 'http://13.233.90.79:3000/',
     GOOGLE_URL : 'https://accounts.google.com/o/oauth2/v2/auth?',
     razorKey:"rzp_test_I5igMZcnHQiPyk",
     paypalClientId:'AdTF00jjUqvWJuDk1wO2JKtxtQq8FKGl6z1kF7kABDBPeDnL0txFhhZ8-fK3SoE5pd6ndlyxtML2qoXE'
