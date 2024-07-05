64be8936731b946b74a7447c8b413b5f4a8fccd7
Shanushajahan707 <shanushajahan707@gmail.com>
2024-07-05 10:21:10 +0530

    socket backed hosted url set

--- src/app/service/user/socket/socket.service.ts
+++ src/app/service/user/socket/socket.service.ts
@@ -32,7 +32,7 @@ export class SocketService {
   });
 
   constructor() {
-    this._socket = io('http://localhost:3000', {});
+    this._socket = io('https://capturelive-shanushajahan707s-projects.vercel.app', {});
     this._socket.on('connect_error', (error) => {
       console.error('Socket connection error:', error);
     });
