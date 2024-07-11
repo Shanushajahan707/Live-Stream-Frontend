74f8f1b1070272984a005034bcd1c1c5e9601f4f
Shanushajahan707 <shanushajahan707@gmail.com>
2024-07-11 11:38:30 +0530

    url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/',
-  // apiUrl: 'https://onlineecart.shop/',
+  // apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://onlineecart.shop/',
   GOOGLE_URL: 'https://accounts.google.com/o/oauth2/v2/auth?',
   razorKey: 'rzp_test_I5igMZcnHQiPyk',
   paypalClientId:
