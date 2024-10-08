cc33c8e53df0f33514eabbe7a6e4fd28cc730c7a
Shanushajahan707 <shanushajahan707@gmail.com>
2024-10-08 19:22:39 +0530

    ack method called in landing page

--- src/app/component/landinghome/landinghome.component.ts
+++ src/app/component/landinghome/landinghome.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../service/user/account/account.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-landinghome',
   templateUrl: './landinghome.component.html',
-  styleUrl: './landinghome.component.scss'
+  styleUrl: './landinghome.component.scss',
 })
-export class LandinghomeComponent {
+export class LandinghomeComponent implements OnInit {
+  private readonly $destroy = new Subject<void>();
+  acknowledge!: boolean;
+  constructor(private _service: AccountService) {}
 
+  ngOnInit(): void {
+    this.ack()
+  }
+
+  ack() {
+    this._service
+      .ack()
+      .pipe(takeUntil(this.$destroy))
+      .subscribe({
+        next: (res) => {
+          if (res && res.ack) {
+            console.log('ack response', res);
+            this.acknowledge = res.ack;
+          }
+        },
+        error: (err) => {
+          console.log(err.error);
+        },
+      });
+  }
 }
