bd7389c58a8ef3cfb345d51c16bb498d78c78362
Shanushajahan707 <shanushajahan707@gmail.com>
2025-06-05 20:53:19 +0530

    apirul updated

--- src/app/service/user/socket/socket.service.ts
+++ src/app/service/user/socket/socket.service.ts
@@ -559,6 +559,10 @@ export class SocketService {
     //     transports: ['websocket'],
     //     withCredentials: true,
     //   } }),
+    // this._socket = io('http://localhost:3000', {
+    //   transports: ['websocket'],
+    //   withCredentials: true,
+    // });
     this._socket = io('https://capturelive.onrender.com', {
       transports: ['websocket'],
       withCredentials: true,
